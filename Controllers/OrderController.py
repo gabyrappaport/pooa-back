ab6ac7ebe73542f36236e28a654422228740b9b9
Alix <alix.mallard@gmail.com>
2018-10-17 11:19:58 +0200

    add creation_date, remarques and total_amount

--- Controllers/OrderController.py
+++ Controllers/OrderController.py
@@ -3,6 +3,7 @@ import datetime
 import werkzeug
 from flask import request
 from flask_restful import Resource
+from flask_restful.utils.cors import crossdomain
 
 from Controllers.Helper.HttpResponse import HttpResponse, HttpStatus
 from DataBase.OrderDataBase import *
@@ -38,8 +39,8 @@ class OrderController(Resource):
     def post(self):
         try:
             data = request.get_json(force=True)
-            order = Order(int(data["supplier"]),
-                          int(data["client"]),
+            order = Order(int(data["id_supplier"]),
+                          int(data["id_client"]),
                           datetime.datetime.strptime(data["expected_delivery_date"], "%d-%m-%Y").date(),
                           str(data["payment_type"]),
                           str(data["l_dips"]),
@@ -103,6 +104,7 @@ class OrderController(Resource):
         except (ValueError, WritingDataBaseError, KeyError, werkzeug.exceptions.BadRequest) as e:
             return HttpResponse(HttpStatus.Bad_Request, message=str(e)).get_response()
 
+    @crossdomain(origin='*')
     def delete(self):
         try:
             data = request.get_json(force=True)
